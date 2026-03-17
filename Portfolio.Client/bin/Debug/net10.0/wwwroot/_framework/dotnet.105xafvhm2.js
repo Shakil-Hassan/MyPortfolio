cc4cdac3616e553444be13923f5bf360a98dca18
Shakil-Hassan <shakilhassan.bcm14@gmail.com>
2026-03-18 02:03:06 +0530

    Added  truncate the description to 120 chars with a "Read more" toggle per card.

--- Portfolio.Client/bin/Debug/net10.0/wwwroot/_framework/dotnet.105xafvhm2.js
+++ Portfolio.Client/bin/Debug/net10.0/wwwroot/_framework/dotnet.105xafvhm2.js
@@ -5,7 +5,7 @@ var e=!1;const t=async()=>WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,
   "mainAssemblyName": "Portfolio.Client",
   "applicationEnvironment": "Development",
   "resources": {
-    "hash": "sha256-yOcuBBm+KCbym/ounqkeg1ZPwitpJ38tAXO7eL/x3mw=",
+    "hash": "sha256-hUEC/fi6rZWr29QgG2PCnXOF77M4t3QsSmZKRRvgm0w=",
     "jsModuleNative": [
       {
         "name": "dotnet.native.ykrnppwhq2.js"
@@ -1242,16 +1242,16 @@ var e=!1;const t=async()=>WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,
       },
       {
         "virtualPath": "Portfolio.Client.wasm",
-        "name": "Portfolio.Client.l98zs5b06t.wasm",
-        "integrity": "sha256-Z2sUia0upK+3MMqO/3j/LVUOXJn+qd+6huBtxp7Ky4g=",
+        "name": "Portfolio.Client.1bsidoqq8h.wasm",
+        "integrity": "sha256-zQxwRoaOJ0tY+PLB8EBFRa0Gmdf6OgePT6/8PghZxgQ=",
         "cache": "force-cache"
       }
     ],
     "pdb": [
       {
         "virtualPath": "Portfolio.Client.pdb",
-        "name": "Portfolio.Client.kjcz0ofxnl.pdb",
-        "integrity": "sha256-QLr7zmIzO/A7DyhyoFj4YnwTzFpbFbfgC2trzs/1RHw=",
+        "name": "Portfolio.Client.68u3pjo0uh.pdb",
+        "integrity": "sha256-Ot+280PuK+O+Bj2IdJvzidbMi60cEOETsaINSNFn79M=",
         "cache": "force-cache"
       }
     ],
--- Portfolio.Client/obj/Debug/net10.0/Portfolio.Client.AssemblyInfo.cs
+++ Portfolio.Client/obj/Debug/net10.0/Portfolio.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b012d828ebd665271e75d54729c7d7ce3656e5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08d2fb0f155ae1cd3ff40c1a1a454cf1bff0e96e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
